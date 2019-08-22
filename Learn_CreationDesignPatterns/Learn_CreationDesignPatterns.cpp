1af72440fb62e5099f09d67f3d6f8d726243b492
Lokesh <lokeshn@chetu.com>
2019-08-22 05:39:31 +0530

    Added new Abstract factory concept challenge and solution

--- Learn_CreationDesignPatterns/Learn_CreationDesignPatterns.cpp
+++ Learn_CreationDesignPatterns/Learn_CreationDesignPatterns.cpp
@@ -118,7 +118,7 @@ public:
 
 };
 
-int main() {
+int mainAbstrachFactoryMethod() {
 
 	CarFactory* carPlant;
 
@@ -155,14 +155,10 @@ int main() {
 		car1door->Open();
 		car1Engine->Run();
 	}
-
-
-
 	return 0;
 }
 
 #pragma endregion
-
 
 #pragma region Factor Method Concept
 
@@ -283,6 +279,271 @@ int mainFactoryMethodConcept()
 	return 0;
 }
 #pragma endregion
+
+#pragma region Challange
+
+// 04_06_AbFacChallenge_Start.cpp 
+//
+#include <windows.h>   // WinApi header
+
+#pragma region Backgrounds
+// Base Background class
+class Background
+{
+protected:
+	char _theme[10];
+public:
+	Background()
+	{
+	}
+	virtual void getTheme() = 0;
+};
+
+// Derived Light Background class
+class LightBkgd : public Background
+{
+public:
+	LightBkgd(HANDLE hConsole)
+	{
+		SetConsoleTextAttribute(hConsole, 240);
+		cout << "Making the background white" << endl;
+		strcpy_s(_theme, "light");
+	}
+	void getTheme()
+	{
+		cout << "This is a part of the light theme" << endl;
+	}
+};
+
+// Derived Dark Background class
+class DarkBkgd : public Background
+{
+public:
+	DarkBkgd(HANDLE hConsole)
+	{
+		SetConsoleTextAttribute(hConsole, 15);
+		cout << "Making the background black" << endl;
+		strcpy_s(_theme, "dark");
+	}
+	void getTheme()
+	{
+		cout << "This is a part of the dark theme" << endl;
+	}
+};
+#pragma endregion Background
+
+#pragma region Text
+// Base Text class
+class Text
+{
+protected:
+	char _theme[10];
+public:
+	Text()
+	{
+	}
+	virtual void message(HANDLE hConsole) = 0;
+};
+
+// Derived Black Text class
+class BlackText : public Text
+{
+public:
+	BlackText(HANDLE hConsole)
+	{
+		SetConsoleTextAttribute(hConsole, 240);
+		strcpy_s(_theme, "light");
+		cout << "Making the text black" << endl;
+	}
+	void message(HANDLE hConsole)
+	{
+		SetConsoleTextAttribute(hConsole, 240);
+		cout << "This is the default font from the light theme" << endl;
+	}
+};
+
+// Derived White Text class
+class WhiteText : public Text
+{
+public:
+	WhiteText(HANDLE hConsole)
+	{
+		SetConsoleTextAttribute(hConsole, 15);
+		strcpy_s(_theme, "dark");
+		cout << "Making the text white" << endl;
+	}
+	void message(HANDLE hConsole)
+	{
+		SetConsoleTextAttribute(hConsole, 15);
+		cout << "This is the default font for the dark theme" << endl;
+	}
+};
+
+// Derived Dark Green Text class
+class DarkGreenText : public Text
+{
+public:
+	DarkGreenText(HANDLE hConsole)
+	{
+		SetConsoleTextAttribute(hConsole, 242);
+		strcpy_s(_theme, "light");
+		cout << "Making the text darkGreen" << endl;
+	}
+	void message(HANDLE hConsole)
+	{
+		SetConsoleTextAttribute(hConsole, 242);
+		cout << "This is the new dark green font from the light theme" << endl;
+	}
+};
+
+// Derived Light Green Text class
+
+class LightGreenText : public Text
+{
+public:
+	LightGreenText(HANDLE hConsole)
+	{
+		SetConsoleTextAttribute(hConsole, 10);
+		strcpy_s(_theme, "dark");
+		cout << "Making the text light green" << endl;
+	}
+	void message(HANDLE hConsole)
+	{
+		SetConsoleTextAttribute(hConsole, 10);
+		cout << "This is the new light green font for the dark theme" << endl;
+	}
+};
+
+#pragma endregion Text
+
+#pragma region Factories
+// Base Factory
+class ThemeFactory
+{
+public:
+	virtual Background* setBackground(HANDLE hConsole) = 0;
+	virtual Text* getDefaultText(HANDLE hConsole) = 0;
+
+};
+
+// Derived Light Theme Factory which pairs the white background with darker text
+class LightFactory : public ThemeFactory
+{
+public:
+	Background * setBackground(HANDLE hConsole)
+	{
+		return new LightBkgd(hConsole);
+	}
+	Text * getDefaultText(HANDLE hConsole)
+	{
+		return new BlackText(hConsole);
+	}
+
+};
+
+// Derived Light Theme Factory which pairs the new green background with darker text
+class LightGreenFactory : public ThemeFactory
+{
+public:
+	Background * setBackground(HANDLE hConsole)
+	{
+		return new LightBkgd(hConsole);
+	}
+	Text * getDefaultText(HANDLE hConsole)
+	{
+		return new DarkGreenText(hConsole);
+	}
+
+};
+
+// Derived Dark Theme Factory which pairs the black background with lighter text
+class DarkFactory : public ThemeFactory
+{
+public:
+	Background * setBackground(HANDLE hConsole)
+	{
+		return new DarkBkgd(hConsole);
+	}
+	Text * getDefaultText(HANDLE hConsole)
+	{
+		return new WhiteText(hConsole);
+	}
+
+};
+
+// Derived Dark Theme Factory which pairs the green background with lighter text
+class DarkGreenFactory : public ThemeFactory
+{
+public:
+	Background * setBackground(HANDLE hConsole)
+	{
+		return new DarkBkgd(hConsole);
+	}
+	Text * getDefaultText(HANDLE hConsole)
+	{
+		return new LightGreenText(hConsole);
+	}
+
+};
+#pragma endregion Factories
+
+int main()
+{
+	// Configure console variables and colours
+	HANDLE  hConsole;
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, 30);
+
+	ThemeFactory* app;
+	int choice;
+
+	cout << "Select a theme: " << endl;
+	cout << "1: Light" << endl;
+	cout << "2: Dark" << endl;
+	cout << "3: Light with green" << endl;
+	cout << "4: Dark with green " << endl;
+	cout << "Selection: ";
+	cin >> choice;
+	cout << endl;
+
+	// Initiate the correct factory based on user input
+	switch (choice)
+	{
+	case 1:
+		app = new LightFactory;
+		break;
+	case 2:
+		app = new DarkFactory;
+		break;
+	case 3:
+		app = new LightGreenFactory;
+		break;
+	case 4:
+		app = new DarkGreenFactory;
+		break;
+	default:
+		cout << "Invalid Selection" << endl;
+		app = NULL;
+		break;
+	}
+
+	// Get the correct objects based on factory selected
+	if (app != NULL)
+	{
+		Background* appBkgd = app->setBackground(hConsole);
+		Text *appDefaultText = app->getDefaultText(hConsole);
+
+
+		appDefaultText->message(hConsole);
+
+	}
+
+	cout << endl;
+	system("pause");
+}
+
+#pragma endregion
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
